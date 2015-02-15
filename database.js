18c34f3fb42f283dee745bc7a5edabe465a9bf64
Clayton A. Alves <clayton.aa@gmail.com>
2015-02-14 23:42:45 -0300

    Adicionei a leitura de criptografia na autenticação. Atualizei o módulo database para usar vigo9

--- database.js
+++ database.js
@@ -1,11 +1,14 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection('mysql://admmysql:1234@localhost/vigo');
-//var connection = mysql.createConnection('mysql://root:@localhost/vigo');
+var crypt = require('./criptografia.js');
+
+//var connection = mysql.createConnection('mysql://admmysql:1234@localhost/vigo');
+var connection = mysql.createConnection('mysql://root:@localhost/vigo_erp');
 
 connection.connect();
 
-exports.autentica = function(user, password, callback) {
-    connection.query('select login, senha from operadores where login=? and senha=?', [user, password], function(err, rows, fields) {
+exports.autentica = function (user, password, callback) {
+    var encrypted_password = crypt.encrypt(password);
+    connection.query('select login, senha from sistema_operadores where login=? and senha=?', [user, encrypted_password], function(err, rows, fields) {
         // Se o numero de rows nao for igual a zero (significa que o registro foi encontrado)
         var result = !(rows.length===0);
         callback(null, result);
@@ -14,34 +17,21 @@ exports.autentica = function(user, password, callback) {
 
 // Alguns clientes podem ter o mesmo nome e CNPJ, então a View "usuarios_unicos"
 // serve para eliminar as duplicidades.
-exports.notas = function(dataInicial, dataFinal, empresa, servico, callback) {
-    var sql =  "SELECT DISTINCT                                                            " +
-               "   nf.numero,                                                              " +
-               "   dt_emissao as emissao,                                                  " +
-               "   nf.nome,                                                                " +
-               "   nf.valor_servicos as valor,                                             " +
-               "   us.cpfcgc                                                               " +
-               "FROM nf                                                                    " +
-               "LEFT JOIN usuarios_unicos us on (nf.nome=us.nome AND nf.cpfcnpj=us.cpfcgc) " +
-               "LEFT JOIN empresas emp ON nf.idempresa=emp.id                              " +
-               "WHERE (dt_emissao BETWEEN '" + dataInicial + "' AND '" + dataFinal + "')   " +
-               "   AND emp.fantasia='" + empresa + "'" +
-               "   AND nf.natureza='" + servico + "'";
+exports.notas = function (dataInicial, dataFinal, empresa, servico, callback) {
+    var sql =  "SELECT id, emissao, nossonumero, nome, cpfcgc, valor             " +
+               "FROM financeiro_boletos                                          " +
+               "WHERE                                                            " +
+               "   emissao BETWEEN '" + dataInicial + "' AND '" + dataFinal + "' " +
+               "   AND id_empresa='" + empresa + "'                              " +
+               "LIMIT 10";
 
     connection.query(sql, callback);
 }
 
 exports.empresas = function (callback) {
-    var sql = "select id, fantasia from empresas";
-
+    var sql = "select id, fantasia from sistema_empresas";
     connection.query(sql, function (err, rows, fields) {
         callback(rows);
     });
 }
 
-exports.servicos = function (callback) {
-    var sql = "select distinct natureza from nf";
-    connection.query(sql, function (err, rows, fields) {
-        callback(rows);
-    });
-}
