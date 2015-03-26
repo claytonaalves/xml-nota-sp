6baef8c719a5693fcf0eb4afed671d25b33903ad
Clayton A. Alves <clayton.aa@gmail.com>
2015-03-25 21:17:42 -0400

    Adequação dos caminhos para a user interface em www

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
 var express = require('express');
-var moment = require('./moment.js');
-var baixarNotas = require('./baixanotas.js').baixar;
-var database = require('./database.js');
+var moment = require('./lib/moment.js');
+var notas = require('./lib/baixanotas.js');
+var database = require('./lib/database.js');
 var config = require('./config.js');
+var db = require('./dbconfig.js');
 var app = express();
 
 var nodeUserGid = "node";
 var nodeUserUid = "node";
+var port = 8010;
 
-process.chdir('/var/www/app');
+database.configure(db.host, db.username, db.password, db.database);
+
+//process.chdir('/var/www/app');
 
 app.use(express.bodyParser());
 app.use(express.logger());
-app.use(express.favicon(__dirname + '/img/favicon.ico'));
-app.use('/static', express.static(__dirname + '/public'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/img', express.static(__dirname + '/img'));
+app.use(express.favicon(__dirname + '/www/img/favicon.ico'));
+app.use('/static', express.static(__dirname + '/www/public'));
+app.use('/css', express.static(__dirname + '/www/css'));
+app.use('/js', express.static(__dirname + '/www/js'));
+app.use('/img', express.static(__dirname + '/www/img'));
 
 var auth = express.basicAuth(database.autentica);
 
 app.get('/', auth, function (req, res) {
-    res.sendfile('views/index.html');
+    res.sendfile('www/views/index.html');
 });
 
 app.get('/views/:arquivo', function (req, res) {
-    res.sendfile('views/' + req.params.arquivo);
+    res.sendfile('www/views/' + req.params.arquivo);
 });
-
-
-app.get('/notas', function (req, res) {
-    var data1, data2, q = req.query;
-
-    data1 = moment(q.dataInicial, "DD/MM/YYYY").format("YYYY-MM-DD");
-    data2 = moment(q.dataFinal, "DD/MM/YYYY").format("YYYY-MM-DD");
-
-    database.notas(data1, data2, q.empresa, q.servico, function (err, rows, fields) {
-        if (err) throw err;
-        res.send(rows);
-    });
-});
-
 
 app.get('/empresas', function (req, res) {
     database.empresas(function(rows) {
@@ -48,22 +38,20 @@ app.get('/empresas', function (req, res) {
     });
 });
 
+app.get('/notas', function (req, res) {
+    var q = req.query;
+    var data1 = moment(q.dataInicial, "DD/MM/YYYY").format("YYYY-MM-DD");
+    var data2 = moment(q.dataFinal, "DD/MM/YYYY").format("YYYY-MM-DD");
 
-app.get('/servicos', function (req, res) {
-    database.servicos(function(rows) {
+    database.notas(data1, data2, q.empresa, q.servico, function (err, rows) {
+        if (err) throw err;
         res.send(rows);
     });
 });
 
-
 // Usado por um EventSource
 app.get('/xml/gerar', function(req, res)  {
-    var data1, data2, empresa, servico, q = req.query;
-
-    empresa = unescape(q.empresa);
-    servico = unescape(q.servico);
-    data1 = moment(q.dataInicial, "DD/MM/YYYY").format("YYYY-MM-DD");
-    data2 = moment(q.dataFinal, "DD/MM/YYYY").format("YYYY-MM-DD");
+    var q = req.query;
 
     req.socket.setTimeout(Infinity);
  
@@ -73,8 +61,8 @@ app.get('/xml/gerar', function(req, res)  {
         'Connection': 'keep-alive'
     });
     res.write('\n');
- 
-    baixarNotas(data1, data2, empresa, servico, function (linha) {
+
+    notas.gerar(q.numeros, function (linha) {
         var d = new Date();
         res.write('id: ' + d.getMilliseconds() + '\n');
         res.write('data:' + linha +   '\n\n');
@@ -83,13 +71,12 @@ app.get('/xml/gerar', function(req, res)  {
         // Enviando evento END
         res.write('id: ' + d.getMilliseconds() + '\n');
         res.write('event: end\n');
-        res.write('data: ' + filename + '.xml\n\n');
+        res.write('data: ' + filename + '\n\n');
     });
 });
 
-
 app.get('/xml/baixar/:arquivo', function (req, res) {
-    res.download(req.params.arquivo);
+    res.download('notas/'+req.params.arquivo);
 });
 
 app.get('/config', function (req, res) {
@@ -101,8 +88,10 @@ app.post('/config', function (req, res) {
     res.send('ok');
 });
 
-app.listen(8000, function() {
+app.listen(port /* , function() {
 	process.setgid(nodeUserGid);
 	process.setuid(nodeUserUid);
-});
+} */ );
+
+console.log('Servidor iniciado... http://localhost:'+port+'/');
 
